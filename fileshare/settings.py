dadf6908340fc7e699130378240470809d65bd53
ell <ell@ell.dev>
2023-12-22 21:57:25 -0600

    add logging

--- fileshare/settings.py
+++ fileshare/settings.py
@@ -138,3 +138,17 @@ STORAGES = {
         },
     },
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
